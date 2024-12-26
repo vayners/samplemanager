a738e6deb4f1175151ae044384be98a08656ea6d
sv <s@vayner.de>
2024-12-26 01:42:32 +0100

    Initial Commit

---
+++ sampleman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleman
+//
+//  Created by Serge Vayner on 26.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleman/samplemanApp.swift
@@ -0,0 +1,17 @@
+//
+//  samplemanApp.swift
+//  sampleman
+//
+//  Created by Serge Vayner on 26.12.24.
+//
+
+import SwiftUI
+
+@main
+struct samplemanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
